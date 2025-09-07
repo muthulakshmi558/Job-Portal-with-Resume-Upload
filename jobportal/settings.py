d3a18f590ef8a42aebb4b83e41d9249cd4758e38
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-09-07 20:00:01 +0530

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "job-portal-with-resume-upload.onrender.com",
+    "job-portal-with-resume-upload-1.onrender.com",
 ]
 
 # Application definition
